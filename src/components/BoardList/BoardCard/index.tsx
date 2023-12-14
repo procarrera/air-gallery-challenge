19003c61cec72cbfd91220ce9db1e20b0b2ac3c2
Pedro Carrera <procarrera@gmail.com>
2023-12-14 17:59:53 -0300

    Refactor BoardItem component to display a wind icon when there are no thumbnails

--- src/components/BoardList/BoardCard/index.tsx
+++ src/components/BoardList/BoardCard/index.tsx
@@ -33,15 +33,20 @@ export interface SubBoardItem {
 interface BoardItemProps {
     board: SubBoardItem;
 }
+
+import { Wind  } from 'lucide-react';
+
 export default function BoardItem({ board }: BoardItemProps) {
     return (
-        <li className="w-full sm:w-60 h-60 rounded overflow-hidden shadow-lg relative">
+        <li className="w-full sm:w-60 h-60 rounded overflow-hidden shadow-lg relative hover:border">
             <div className="h-full">
                 {board.thumbnails && (
                     <img className="w-full h-full object-cover" src={board.thumbnails[0]} alt="Sunset in the mountains" />
                 )}
                 {!board.thumbnails && (
-                    <div className="w-full h-full bg-slate-200"></div>
+                    <div className="w-full h-full bg-gray-50 flex content-center items-center justify-center">
+                        <Wind  strokeWidth={1} size={64} color="#c6c6c6" />
+                    </div>
                 )}
             </div>
             <h3 className="text-white absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-2 text-center">{board.title}</h3>
